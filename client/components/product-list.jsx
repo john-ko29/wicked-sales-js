b79abfad8d8179279b4c63854e20e9fa6134f93f
John Ko <jko121790@gmail.com>
2020-04-23 16:43:05 -0700

    created product list component and getProduct method

---
+++ client/components/product-list.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          products: data
+        });
+      });
+  }
+}
+
+export default ProductList;
