4b7d9bbbb5ee2ae60313b3f8f5e53f58cdbe3bcc
John Ko <jko121790@gmail.com>
2020-04-23 16:25:59 -0700

    added header components

---
+++ client/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <header>
+      <h3 className='text-light bg-dark'><span className='font-weight-bold'>$</span>Wicked Sales</h3>
+    </header>
+  );
+}
+
+export default Header;
--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import Header from './components/header';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <Header />
+    <App />
+  </div>,
   document.querySelector('#root')
 );
