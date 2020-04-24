68b87ba8d2f14cefe1c78400f08b98026fca09c0
John Ko <jko121790@gmail.com>
2020-04-24 14:57:14 -0700

    added getCartItems method and called it in the componentDidMount

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,15 +6,31 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: { name: 'catalog', params: {} }
+      view: { name: 'catalog', params: {} },
+      cart: []
     };
     this.setView = this.setView.bind(this);
+    this.getCartItems = this.getCartItems.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCartItems();
   }
 
   setView(name, params) {
     this.setState({
       view: { name: name, params: params }
     });
+  }
+
+  getCartItems() {
+    fetch('/api/carts')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          cart: data
+        });
+      });
   }
 
   render() {
