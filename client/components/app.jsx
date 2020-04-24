4dd79d4e1dc34f986e5c28a7f8c6d922263d217e
John Ko <jko121790@gmail.com>
2020-04-24 16:05:15 -0700

    setView to header to access CartSummary page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -64,7 +64,7 @@ export default class App extends React.Component {
     }
     return (
       <div>
-        <Header cartItemCount={this.state.cart.length} />
+        <Header cartItemCount={this.state.cart.length} setView={this.setView} />
         {productPage}
       </div>
     );
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ function Header(props) {
   return (
     <header className='text-light bg-dark d-flex justify-content-between'>
       <h3 className='ml-5'><span className='font-weight-bold'>$</span>Wicked Sales</h3>
-      <h3 className='mr-5'>{props.cartItemCount} <i className='fas fa-shopping-cart'></i></h3>
+      <h3 className='mr-5' onClick={() => props.setView('cart', {})}>{props.cartItemCount} <i className='fas fa-shopping-cart'></i></h3>
     </header>
   );
 }
