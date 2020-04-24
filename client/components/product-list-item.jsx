d3f4cfdc218b0aee195eb063092b4058fdba2937
John Ko <jko121790@gmail.com>
2020-04-23 17:33:36 -0700

    css edited

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function productListItem(props) {
   return (
     <div className="card my-4">
-      <img src={props.image} className='img-thumbnail' />
+      <img src={props.image} className='cardImg' />
       <div className="card-body">
         <h5 className='class-title'>{props.name}</h5>
         <h5 className='text-secondary'>${props.price}</h5>
