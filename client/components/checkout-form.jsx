4ff1fe6106cdcf96937806ecb7487a4b341c566d
John Ko <jko121790@gmail.com>
2020-04-24 19:43:21 -0700

    added links back to catalog and placeOrder

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -14,6 +14,19 @@ class CheckoutForm extends React.Component {
     return (
       <div className='m-4'>
         <h1>My Cart</h1>
+        <h3 className='text-secondary'>Order Total: ${this.props.totalPrice}</h3>
+        <form className="m-4">
+          <label className="d-block font-weight-bold" htmlFor="name">Name</label>
+          <input className="w-100 mb-3" type="text" id="name" name="name" />
+          <label className="d-block font-weight-bold" htmlFor="creditCard">Credit Card</label>
+          <input className="w-100 mb-3" type="text" id="creditCard" name="creditCard" />
+          <label className="d-block font-weight-bold" htmlFor="address">Shipping Address</label>
+          <textarea className="w-100 mb-3" name="address" id="address" rows="4"></textarea>
+        </form>
+        <div className='d-flex justify-content-between'>
+          <p className='text-secondary mb-3' onClick={() => this.props.setView('catalog', {})}>&lt; Continue Shopping</p>
+          <button className='btn btn-primary w-25'>Place Order</button>
+        </div>
       </div>
     );
   }
