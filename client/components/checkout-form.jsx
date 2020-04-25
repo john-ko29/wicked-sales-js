ca0f910dad044a97f98f832e87ee3f13c6e1e49c
John Ko <jko121790@gmail.com>
2020-04-24 20:23:56 -0700

    place order front end complete

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -9,6 +9,8 @@ class CheckoutForm extends React.Component {
       shippingAddress: ''
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
   handleChange(event) {
@@ -23,23 +25,29 @@ class CheckoutForm extends React.Component {
     }
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.placeOrder(this.state);
+  }
+
   render() {
     return (
       <div className='m-4'>
         <h1>My Cart</h1>
         <h3 className='text-secondary'>Order Total: ${this.props.totalPrice}</h3>
-        <form className="m-4">
+        <form className="m-4" onSubmit={this.handleSubmit}>
           <label className="d-block font-weight-bold" htmlFor="name">Name</label>
           <input className="w-100 mb-3" onChange={this.handleChange} type="text" id="name" name="name" />
           <label className="d-block font-weight-bold" htmlFor="creditCard">Credit Card</label>
           <input className="w-100 mb-3" onChange={this.handleChange} type="text" id="creditCard" name="creditCard" />
           <label className="d-block font-weight-bold" htmlFor="address">Shipping Address</label>
           <textarea className="w-100 mb-3" onChange={this.handleChange} name="address" id="address" rows="4"></textarea>
+          <div className='d-flex justify-content-between'>
+            <p className='text-secondary mb-3' onClick={() => this.props.setView('catalog', {})}>&lt; Continue Shopping</p>
+            <input type="submit" value="Place Order" className='btn btn-primary' />
+          </div>
         </form>
-        <div className='d-flex justify-content-between'>
-          <p className='text-secondary mb-3' onClick={() => this.props.setView('catalog', {})}>&lt; Continue Shopping</p>
-          <button className='btn btn-primary w-25'>Place Order</button>
-        </div>
+
       </div>
     );
   }
