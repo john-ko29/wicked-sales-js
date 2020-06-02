07e96f3d51700e24423efe3a5d0f69235d427490
John Ko <jko121790@gmail.com>
2020-06-02 11:49:52 -0700

    updated css

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -29,9 +29,6 @@ class CheckoutForm extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    if (this.state.name === '' || this.state.creditCard === '' || this.state.shippingAddress === '') {
-      return;
-    }
     this.props.placeOrder(this.state);
   }
 
@@ -55,13 +52,13 @@ class CheckoutForm extends React.Component {
         <h3 className='text-secondary'>Order Total: ${this.props.totalPrice}</h3>
         <form className="m-4" onSubmit={this.handleSubmit}>
           <label className="d-block font-weight-bold" htmlFor="name">Name</label>
-          <input className="form-control w-100 mb-3" onChange={this.handleChange} type="text" id="name" name="name" />
+          <input className="form-control w-100 mb-3" onChange={this.handleChange} required type="text" id="name" name="name" />
           <label className="d-block font-weight-bold" htmlFor="creditCard">Credit Card</label>
-          <input className="form-control w-100 mb-3" onChange={this.handleChange} type="text" id="creditCard" name="creditCard" />
+          <input className="form-control w-100 mb-3" onChange={this.handleChange} required type="text" id="creditCard" name="creditCard" />
           <label className="d-block font-weight-bold" htmlFor="address">Shipping Address</label>
-          <textarea className="form-control w-100 mb-3" onChange={this.handleChange} name="address" id="address" rows="4"></textarea>
+          <textarea className="form-control w-100 mb-3" onChange={this.handleChange} required name="address" id="address" rows="4"></textarea>
           <input type="checkbox" onClick={this.handleConfirm} id='confirmed' name='confirmed' className='mr-1'/>
-          <label htmlFor="confirmed">&#9;I understand that no real purchase is being made here, and
+          <label className='inline' htmlFor="confirmed">&#9;I understand that no real purchase is being made here, and
           that I shoud and will not use any personal or sensitive information above.</label>
           <div className='d-flex justify-content-between'>
             <p className='pointer text-secondary mb-3' onClick={() => this.props.setView('catalog', {})}>&lt; Continue Shopping</p>
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
   return (
     <header className='header text-light d-flex justify-content-between'>
-      <h3 className='pointer ml-5' onClick={() => props.setView('catalog', {})}><img className='choco-image' src='/images/chocobo-icon.jpeg'></img>Choco Shop</h3>
+      <h3 className='pointer ml-5' onClick={() => props.setView('catalog', {})}><img className='choco-image' src='/images/chocobo.png'></img>Choco Shop</h3>
       <h3 className='pointer mr-5' onClick={() => props.setView('cart', {})}>{props.cartItemCount} <i className='fas fa-shopping-cart'></i></h3>
     </header>
   );
