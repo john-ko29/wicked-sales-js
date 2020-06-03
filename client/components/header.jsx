80884e3557b729adddf909227957a44141080262
John Ko <jko121790@gmail.com>
2020-06-03 16:25:44 -0700

    css updates

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <header className='header text-light d-flex justify-content-between'>
-      <h3 className='pointer ml-5' onClick={() => props.setView('catalog', {})}><img className='choco-image' src='/images/chocobo.png'></img>Choco Shop</h3>
+    <header className='header text-light d-flex justify-content-between align-items-center'>
+      <h3 className='pointer ml-5' onClick={() => props.setView('catalog', {})}><img className='choco-image' src='/images/chocobo.png'></img>Chocobo Shop</h3>
       <h3 className='pointer mr-5' onClick={() => props.setView('cart', {})}>{props.cartItemCount} <i className='fas fa-shopping-cart'></i></h3>
     </header>
   );
--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function productListItem(props) {
   return (
-    <div className="card productCard my-4" onClick={() => props.setView('details', { productId: props.productId })}>
+    <div className="productCard my-4" onClick={() => props.setView('details', { productId: props.productId })}>
       <img src={props.image} className='cardImg' />
-      <div className="card-body">
+      <div>
         <h5 className='class-title'>{props.name}</h5>
         <h5 className='text-secondary'>${(props.price / 100).toFixed(2)}</h5>
         <p className='card-text'>{props.description}</p>
